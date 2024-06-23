abbd9e4606e6ae1ac3472b464be7f59633904348
Yogeshsingh017 <yogeshsighyogesh54@gmail.com>
2024-06-23 14:31:32 +0530

    message

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        roboto:['Roboto', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
