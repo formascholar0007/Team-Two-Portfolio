1bc23148e6faefb275637348f11ddba44bec86ba
Yogeshsingh017 <yogeshsighyogesh54@gmail.com>
2024-06-26 11:41:01 +0530

    message

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -1,31 +1,30 @@
 function HireMe() {
-    return (
+  return (
       <div className="container mx-auto px-4 sm:px-6 lg:px-9 py-8">
-        <div className="relative h-[50vh] sm:h-[60vh] lg:h-[66vh] flex items-center justify-center bg-cover bg-center">
-          <div
-            className="absolute inset-0 w-full h-full bg-cover bg-center"
-            style={{
-              backgroundImage: "url('https://wallpapercave.com/wp/wp7287123.jpg')",
-              objectFit: 'cover',
-              objectPosition: 'center',
-            }}
-          ></div>
-          <div className="absolute inset-0 bg-green-900 opacity-70"></div>
-          <div className="relative text-center text-white px-4">
-            <h1 className="text-3xl sm:text-5xl lg:text-6xl font-bold">
-              I'm <span className="text-green-400">Available</span> for freelancing
-            </h1>
-            <p className="mt-4 text-lg sm:text-xl">
-              A small river named Duden flows by their place and supplies it with the necessary regelialia.
-            </p>
-            <button className="mt-8 px-6 py-2 sm:px-8 sm:py-3 lg:px-9 lg:py-3 bg-green-500 text-white rounded-full shadow-lg hover:bg-blue-700">
-              HIRE ME
-            </button>
+          <div className="relative h-[50vh] sm:h-[60vh] lg:h-[66vh] flex items-center justify-center bg-cover bg-center">
+              <div
+                  className="absolute inset-0 w-full h-full bg-cover bg-center"
+                  style={{
+                      backgroundImage: "url('https://wallpapercave.com/wp/wp7287123.jpg')",
+                      objectFit: 'cover',
+                      objectPosition: 'center',
+                  }}
+              ></div>
+              <div className="absolute inset-0 bg-green-900 opacity-70"></div>
+              <div className="relative text-center text-white px-4">
+                  <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold">
+                      I'm <span className="text-green-400">Available</span> for freelancing
+                  </h1>
+                  <p className="mt-4 text-base sm:text-lg md:text-xl">
+                      A small river named Duden flows by their place and supplies it with the necessary regelialia.
+                  </p>
+                  <button className="mt-8 px-4 py-2 sm:px-6 sm:py-3 md:px-8 md:py-3 lg:px-10 lg:py-3 bg-green-500 text-white rounded-full shadow-lg hover:bg-blue-700">
+                      HIRE ME
+                  </button>
+              </div>
           </div>
-        </div>
       </div>
-    );
-  }
-  
-  export default HireMe;
-  
+  );
+}
+
+export default HireMe;
